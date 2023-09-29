cd670b1a29599fdfcd5fb95b79741d5fbbd04c7a
mrKhan156 <imrankhan772.bd@gmail.com>
2023-09-29 23:43:05 +0600

    new bar added

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const panels = document.querySelectorAll('.panel');
 
+// 2nd project
+
+const progress = document.querySelector('#progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circles = document.querySelectorAll('.circle');
+
+
+
 //adding active effect to the image
 
 panels.forEach(panel=>{
@@ -15,3 +24,64 @@ panels.forEach(panel=>{
         panel.classList.remove('active');
     })
  }
+ 
+
+
+let currentActive = 1 ;
+
+next.addEventListener('click',()=>{
+    currentActive ++;
+    if(currentActive>circles.length){
+        currentActive = circles.length;
+    }
+
+   update()
+})
+
+
+prev.addEventListener('click',()=>{
+    currentActive --;
+    if(currentActive < 1){
+        currentActive = 1;
+    }
+    update()
+})
+
+function update() {
+
+circles.forEach((circle, idx) =>{
+        if(idx <currentActive){
+            circle.classList.add('active1');
+        } else{
+            circle.classList.remove('active1');
+        }
+        
+     })
+    
+        panels.forEach((panel,idnx)=>{
+            if(idnx < currentActive){
+                removeActiveClass()
+                panel.classList.add('active');
+                
+
+            }else{
+                panel.classList.remove('ctive');
+               
+            }
+        })
+     
+
+    const actives = document.querySelectorAll('.active1');
+
+    progress.style.width = (actives.length - 1) / (circles.length-1) *100 + '%';
+    if(currentActive===1){
+        prev.disabled = true;
+
+    } else if (currentActive === circles.length){
+        next.disabled = true;
+        
+    } else{
+        prev.disabled =false;
+        next.disabled = false;
+    }
+}
